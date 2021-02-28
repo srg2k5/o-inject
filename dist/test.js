ff5225cdcf0d08ce2789d9e635b1025775f52912
Chase <chase@truepill.com>
2021-02-28 16:53:00 -0700

    more testing

--- dist/test.js
+++ dist/test.js
@@ -5,6 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const chai_1 = require("chai");
 const index_1 = __importDefault(require("./index"));
+chai_1.use(require('chai-as-promised'));
 function resolvedOneSecond() {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
@@ -23,24 +24,23 @@ const injectables = {
     someNumber,
     basicFunction
 };
-const subject = {
-    untouched: 500,
-    myScore: `oi{{
-    someNumber
-  }}`,
-    otherScore: `oi{{
-    basicFunction(34)
-  }}`,
-    gameMessages: `oi{{
-    Promise.all([
-      resolvedOneSecond(),
-      resolvedOneSecond(),
-      resolvedOneSecond()
-    ])
-  }}`
-};
 (async () => {
-    const injected = await index_1.default(injectables, subject);
+    const injected = await index_1.default(injectables, {
+        untouched: 500,
+        myScore: `oi{{
+      someNumber
+    }}`,
+        otherScore: `oi{{
+      basicFunction(34)
+    }}`,
+        gameMessages: `oi{{
+      Promise.all([
+        resolvedOneSecond(),
+        resolvedOneSecond(),
+        resolvedOneSecond()
+      ])
+    }}`
+    });
     chai_1.expect(injected).to.eql({
         untouched: 500,
         myScore: 43,
@@ -50,6 +50,30 @@ const subject = {
             "One second resolved promise",
             "One second resolved promise"
         ]
+    });
+    // Don't Allow Requires
+    chai_1.expect(index_1.default(injectables, {
+        shouldError: `oi{{
+      require('crypto')
+    }}`
+    })).to.eventually.rejected;
+    const sampleString = "I am not encoded.";
+    const convertString = (s) => Buffer.from(s, 'utf-8').toString('base64');
+    const bufferResult = await index_1.default({
+        sampleString,
+        convertString,
+        Buffer,
+    }, {
+        base64_a: `oi{{
+      Buffer.from("I am not encoded.", 'utf-8').toString("base64")
+    }}`,
+        base64_b: `oi{{
+      convertString(sampleString)
+    }}`
+    });
+    chai_1.expect(bufferResult).to.eql({
+        base64_a: "SSBhbSBub3QgZW5jb2RlZC4=",
+        base64_b: "SSBhbSBub3QgZW5jb2RlZC4=",
     });
     console.log("All Test Complete.");
 })();
--- src/test.ts
+++ src/test.ts
@@ -1,6 +1,8 @@
-import { expect } from 'chai'
+import { expect, use } from 'chai'
 
 import oInject from './index'
+
+use(require('chai-as-promised'))
 
 function resolvedOneSecond() {
   return new Promise ((resolve, reject) => {
@@ -19,32 +21,29 @@ function basicFunction(int: number) {
   return int + someNumber
 }
 
-
 const injectables = {
   resolvedOneSecond,
   someNumber,
   basicFunction
 }
 
-const subject = {
-  untouched: 500,
-  myScore: `oi{{
-    someNumber
-  }}`,
-  otherScore: `oi{{
-    basicFunction(34)
-  }}`,
-  gameMessages: `oi{{
-    Promise.all([
-      resolvedOneSecond(),
-      resolvedOneSecond(),
-      resolvedOneSecond()
-    ])
-  }}`
-}
-
 ;(async () => {
-  const injected = await oInject(injectables, subject)
+  const injected = await oInject(injectables, {
+    untouched: 500,
+    myScore: `oi{{
+      someNumber
+    }}`,
+    otherScore: `oi{{
+      basicFunction(34)
+    }}`,
+    gameMessages: `oi{{
+      Promise.all([
+        resolvedOneSecond(),
+        resolvedOneSecond(),
+        resolvedOneSecond()
+      ])
+    }}`
+  })
 
   expect(injected).to.eql({
     untouched: 500,
@@ -56,7 +55,36 @@ const subject = {
       "One second resolved promise"
     ]
   })
+
+  // Don't Allow Requires
+
+  expect(oInject(injectables, {
+    shouldError: `oi{{
+      require('crypto')
+    }}`
+  })).to.eventually.rejected
+
+  const sampleString = "I am not encoded."
+  const convertString = (s: string) => Buffer.from(s, 'utf-8').toString('base64')
+
+  const bufferResult = await oInject({
+    sampleString,
+    convertString,
+    Buffer,
+  }, {
+    base64_a: `oi{{
+      Buffer.from("I am not encoded.", 'utf-8').toString("base64")
+    }}`,
+    base64_b: `oi{{
+      convertString(sampleString)
+    }}`
+  })
   
+  expect(bufferResult).to.eql({
+    base64_a: "SSBhbSBub3QgZW5jb2RlZC4=",
+    base64_b: "SSBhbSBub3QgZW5jb2RlZC4=",
+  })
+
   console.log("All Test Complete.")
   
 })()
